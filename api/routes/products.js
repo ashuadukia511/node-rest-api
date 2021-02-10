5e33e596c879eabd5e8eca0e87ddbfeda7b8cf6b
ashuadukia511 <ashuadukia511@gmail.com>
2021-02-10 16:48:42 +0530

    commit on main

--- api/routes/products.js
+++ api/routes/products.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     }
 });
 const fileFilter = (req, file, cb) => {
-    if(file.mimetype === 'image/png' ){
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpeg'){
         cb(null, true);
     }else {
         cb(new Error('Wrong File Type'), false);
